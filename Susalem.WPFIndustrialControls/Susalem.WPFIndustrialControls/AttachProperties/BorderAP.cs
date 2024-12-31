88f5fcc5eca03278280f771b9ae7e986981d0f8d
邵家驹 <987585547@qq.com>
2025-01-01 00:51:00 +0800

    add togglebutton style border transition fan rotation

---
+++ Susalem.WPFIndustrialControls/Susalem.WPFIndustrialControls/AttachProperties/BorderAP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Susalem.WPFIndustrialControls.AttachProperties
+{
+    public class BorderAP
+    {
+        public static double GetStrokeThickness(DependencyObject obj)
+        {
+            return (double)obj.GetValue(StrokeThicknessProperty);
+        }
+
+        public static void SetStrokeThickness(DependencyObject obj, double value)
+        {
+            obj.SetValue(StrokeThicknessProperty, value);
+        }
+
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.RegisterAttached("StrokeThickness", typeof(double), typeof(BorderAP), new PropertyMetadata(2d));
+
+
+
+    }
+}
